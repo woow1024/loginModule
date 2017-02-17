a46e7dce13be49bc1cea4c48af5680bc0893f464
yufang.hu@baidao.com <yufang.hu@baidao.com>
2017-02-17 16:30:39 +0800

    commit code

---
+++ requestLogin/RabbitMq.py
@@ -0,0 +1,73 @@
+ # -*- coding:utf-8 -*-     
+import pika    
+class RabbitMQ:
+    def __init__(self, 
+                 host = '192.168.44.223',
+                 port = 5672,
+                 user = '',
+                 pwd = '',
+                 vhost ='/',
+                 queue = None):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.queue= ''
+        self.vhost = vhost
+        self.prfetch_count = 2
+     
+        #FS.test111_13
+    def connect_mq(self):
+        try:
+            auth = pika.PlainCredentials(self.user, self.pwd)
+            parameters = pika.ConnectionParameters(host=self.host, 
+                                               port=self.port,
+                                               virtual_host=self.vhost,
+                                               credentials=auth
+                                               )
+            self.conn = pika.BlockingConnection(parameters)  
+            self.channel = self.conn.channel() 
+            print 'connect rabbitmq success...'
+        except Exception,e:
+            raise "connect mq error %s" %e
+        
+    def get_channel(self):
+        return self.channel
+    
+    def set_queue_count(self, count):
+        self.prfetch_count = count
+        
+    def start_Consumer(self, exchange, queue, callback, routingKey ='answer'):
+        try:
+           
+            self.channel.queue_declare(queue=queue, durable=True)  
+            self.channel.queue_bind(exchange=exchange,  
+                                        queue=queue, routing_key=routingKey)              
+            self.channel.basic_qos(prefetch_count=self.prfetch_count)
+            self.channel.basic_consume(callback, queue = queue,
+                                               no_ack=False)
+            print "start recvive msg..."
+            self.channel.start_consuming()
+        except Exception, e:
+            print(e)     
+            
+    def start_producer(self, msg, exchange, routing_key='FSReplay'):   
+        try:
+            #json_str = json.dumps(msg)
+            self.channel.exchange_declare(exchange=exchange,type='direct')
+            self.channel.basic_publish(exchange=exchange,
+                                   routing_key=routing_key,
+                                   body=msg,
+                                   properties=pika.BasicProperties(
+                                       #delivery_mode = 2
+                                   ))
+
+        except Exception, e:
+            print e
+            #self.channel.close()
+            
+            print("%s %s",__file__,e)   
+            
+    def confirm(self, ch, method):
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        
---
+++ requestLogin/main.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-  
+
+from RabbitMq import RabbitMQ
+from Redis import RedisDb
+from Http import Http
+from gevent import monkey
+
+from public import redis
+from public import producer
+from public import consumer
+import os
+import sys
+import logging
+import json
+import threading
+import time
+monkey.patch_all()
+
+MQ_VHOST = 'test'
+WORKING_DIR = os.path.dirname(sys.argv[0])
+WORKING_DIR = os.path.dirname(WORKING_DIR)
+LOG_DIR = os.path.join(WORKING_DIR, "log")
+LOG_FILENAME = os.path.join(LOG_DIR, "LoginModule.log")
+
+def mq_loop(ch, method, properties, body):
+    try:
+        http = Http()
+        http.normal_method(body)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception, e:
+        print(e)    
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s : %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        stream=sys.stdout,
+                        )
+
+  
+    console = logging.FileHandler(LOG_FILENAME, "a")
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s : %(message)s',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+    console.setFormatter(formatter)
+    logging.getLogger().addHandler(console)  
+
+    logging.info("LoginModule is runing now, process id %d", os.getpid())
+    
+   
+    try:
+        redis.connect()
+    except Exception, e:
+        logging.info("connet redis %s " %e)
+        print "make sure server is started"
+    
+    try:    
+        #producer.connect_mq()
+        consumer.connect_mq()
+    except Exception,e:
+        logging.error("mq connect error %s" %e)
+    
+    consumer.start_Consumer(exchange='FSExchange1', 
+                            queue='FSCenterBus123', 
+                            callback=mq_loop,
+                            routingKey='answer')
+    
+    
